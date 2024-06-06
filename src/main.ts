a3feb0f948fea51f7a05c0e9eeecc3c86d34a90c
aniladari501@gmail.com <anil@suyay.com.au>
2024-06-06 23:15:33 +0530

    added server code

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,51 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { MongoExceptionFilter } from './Exception-handler/mongo-exception-filter';
-import { TypeORMExceptionFilter } from './Exception-handler/typeorm-exception-filter';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new MongoExceptionFilter());
-  app.useGlobalFilters(new TypeORMExceptionFilter());
-  const options = {
-    origin: ['http://localhost:4200', 'https://dts-ui.vercel.app'],
-    allowedHeaders: ['content-type'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  };
-  app.enableCors(options);
-  await app.listen(3000);
-}
-bootstrap();
-
-//server
-
 // import { NestFactory } from '@nestjs/core';
 // import { AppModule } from './app.module';
-// import * as express from 'express';
-// import { join } from 'path';
+// import { MongoExceptionFilter } from './Exception-handler/mongo-exception-filter';
+// import { TypeORMExceptionFilter } from './Exception-handler/typeorm-exception-filter';
 
 // async function bootstrap() {
 //   const app = await NestFactory.create(AppModule);
-
-//   // CORS options
-//   const corsOptions = {
+//   app.useGlobalFilters(new MongoExceptionFilter());
+//   app.useGlobalFilters(new TypeORMExceptionFilter());
+//   const options = {
 //     origin: ['http://localhost:4200', 'https://dts-ui.vercel.app'],
+//     allowedHeaders: ['content-type'],
 //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: true,
 //     preflightContinue: false,
 //     optionsSuccessStatus: 204,
+//     credentials: true,
 //   };
-
-//   app.enableCors(corsOptions);
-
-//   // Serve static files
-//   app.use('/', express.static(join(__dirname, '..', 'public')));
-
+//   app.enableCors(options);
 //   await app.listen(3000);
 // }
-
 // bootstrap();
+
+//server
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // CORS options
+  const corsOptions = {
+    origin: ['http://localhost:4200', 'https://dts-ui.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+
+  app.enableCors(corsOptions);
+
+  // Serve static files
+  app.use('/', express.static(join(__dirname, '..', 'public')));
+
+  await app.listen(3000);
+}
+
+bootstrap();
