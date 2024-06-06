d470d3241e23a9f41524abb6b54d0e9c12bb2ec2
aniladari501@gmail.com <anil@suyay.com.au>
2024-06-06 23:54:42 +0530

    removed comments

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,3 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { MongoExceptionFilter } from './Exception-handler/mongo-exception-filter';
-// import { TypeORMExceptionFilter } from './Exception-handler/typeorm-exception-filter';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.useGlobalFilters(new MongoExceptionFilter());
-//   app.useGlobalFilters(new TypeORMExceptionFilter());
-//   const options = {
-//     origin: ['http://localhost:4200', 'https://dts-ui.vercel.app'],
-//     allowedHeaders: ['content-type'],
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//     preflightContinue: false,
-//     optionsSuccessStatus: 204,
-//     credentials: true,
-//   };
-//   app.enableCors(options);
-//   await app.listen(3000);
-// }
-// bootstrap();
-
-//server
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
