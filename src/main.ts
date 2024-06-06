d3eb9bc10b5e89be22ebac5f73a8ce634b71e1b6
aniladari501@gmail.com <anil@suyay.com.au>
2024-06-06 23:24:42 +0530

    updated port

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
 
   // CORS options
   const corsOptions = {
-    origin: ['http://localhost:4200', 'https://dts-ui.vercel.app'],
+    origin: ['http://localhost:4200', 'https://digitalcard-gray.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
@@ -45,7 +45,7 @@ async function bootstrap() {
   // Serve static files
   app.use('/', express.static(join(__dirname, '..', 'public')));
 
-  await app.listen(3000);
+  await app.listen(5000);
 }
 
 bootstrap();
